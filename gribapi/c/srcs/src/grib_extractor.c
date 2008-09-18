7cd8287a36d091e2491b0987c09ddb4382bc9852
aubert <aubert@aubert-desktop.(none)>
2008-09-18 14:46:39 +0200

    intermediate commit

--- gribapi/c/srcs/src/grib_extractor.c
+++ gribapi/c/srcs/src/grib_extractor.c
@@ -20,6 +20,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
 #include <getopt.h>
@@ -31,10 +32,84 @@
 #define MAX_VAL_LEN  1024
 
 static char  version_number[] = "0.5";
+static char  default_dir[]    = "/tmp";
 
 static void usage(int status);
 
-static int read_grib(FILE* f,char * filename,double *lats,double *lons,int nbCoords);
+static int read_grib(FILE* gribfile,char* directory,char * grib_filename,double *lats,double *lons,int nbCoords);
+
+/**
+*  @brief  Check if the necessary directories can be created
+*          To follow the other functions I return 1 if correct or 0 if err but I don't like it
+*  @param sPath          The directory
+*
+*  @return   1 if file can be parser, 0 otherwise
+*
+*  @author   guillaume.aubert@ctbto.org
+*/
+int create_directories(char *sPath)
+{
+
+  /* check if the directory already exists if not then try to create it */
+  char opath[1024];
+  char *p;
+  size_t len;
+  char errMsg[256];
+
+  strncpy(opath, sPath, sizeof(opath));
+  len = strlen(opath);
+
+  if(opath[len - 1] == '/')
+  {
+    opath[len - 1] = '\0';
+  }
+
+  for(p = opath; *p; p++)
+  {
+     if(*p == '/') 
+     {
+        *p = '\0';
+        if( access(opath, F_OK) && (strlen(opath) > 0) )
+        {
+		   
+           if (mkdir(opath, S_IRWXU) == -1)
+		   {
+              /* error get its string value with perror */
+			  sprintf(errMsg,"Error when creating directory %s",opath);
+			  perror(errMsg);
+
+			  exit(EXIT_FAILURE);
+
+		   }
+           else
+		   {
+	          fprintf(stdout,"Create directory: %s\n", opath);
+		   }
+
+        }
+        *p = '/';
+     }
+  }
+
+  /* last component of the path /home/a/b */
+  if(access(opath, F_OK) && (strlen(opath) > 0))         /* if path is not terminated with / */
+  {
+    if (mkdir(opath, S_IRWXU) == -1)
+	{
+	   /* error get its string value with perror */
+	   sprintf(errMsg,"Error when creating directory %s ",opath);
+	   perror(errMsg);
+
+	   exit(EXIT_FAILURE);
+	}
+	else
+	{
+	  fprintf(stdout,"Create directory: %s", opath);
+	}
+  }
+
+  return EXIT_SUCCESS;
+}
 
 static int parse_coordinates(char * strCoords,double* lats, double* lons, int * nbCoord)
 {
@@ -104,6 +179,7 @@ int main(int argc, char *argv[])
   int optc;
   int option_index = 0;
   char * coordinates = NULL;
+  char * directory   = NULL;
   double lats[100];
   double lons[100];
   int nbCoords = 0;
@@ -117,12 +193,13 @@ int main(int argc, char *argv[])
 		 {"help",        no_argument, &help_flag,1},
 		 {"version",     no_argument, &version_flag,1},
          {"coordinates", required_argument, 0, 'c'},
+         {"directory"  , required_argument, 0, 'd'},
          {0, 0, 0, 0}
      };
 
      /* getopt_long stores the option index here. */
      
-     optc = getopt_long (argc, argv, "vhc:", long_options, &option_index);
+     optc = getopt_long (argc, argv, "vhc:d:", long_options, &option_index);
      
      /* Detect the end of the options. */
      if (optc == -1)
@@ -145,6 +222,10 @@ int main(int argc, char *argv[])
             printf("Coordinates = %s \n",coordinates);
             parse_coordinates(coordinates,lats,lons,&nbCoords);
             printf("Nb Coords %d\n",nbCoords);
+            break;
+
+          case 'd':
+			directory = optarg;
             break;
      
           case 'h':
@@ -192,29 +273,40 @@ int main(int argc, char *argv[])
       usage(EXIT_FAILURE);
    }
 
+   if (directory == NULL)
+   {
+      directory = default_dir;
+   }
+
+   fprintf (stdout, "generate results in %s\n",directory);
+
+   /* create dir if it doesn't exist */
+   create_directories(directory);
+   
+
    for (; optind < argc; ++optind)
    {
      int fail = 0;
 
-     char *filename = argv[optind];
+     char* filename = argv[optind];
 
      f = fopen(filename,"r");
      if(!f) 
      {
-       perror(filename);
-       exit(1);
+        perror(filename);
+        exit(EXIT_FAILURE);
      }
      printf("--------------------------------------------------------------------------\n");
-     printf("---------------------------- Filename: %s---------------------------\n",argv[i+1]);
+     printf("----------------------Process Filename: %s---------------------------\n",filename);
      printf("--------------------------------------------------------------------------\n");
     
-     read_grib(f,filename,lats,lons,nbCoords);
+     read_grib(f,directory,filename,lats,lons,nbCoords);
    } 
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
-static int read_grib(FILE* gribfile,char * filename,double *lats,double *lons,int nbCoords)
+static int read_grib(FILE* gribfile,char* directory,char * grib_filename,double *lats,double *lons,int nbCoords)
 {
   grib_handle* h=NULL;
   int err=0;
@@ -230,7 +322,6 @@ static int read_grib(FILE* gribfile,char * filename,double *lats,double *lons,in
   size_t timelen=MAX_VAL_LEN;
 
   size_t size=4;
-  /* double lat=-40,lon=15; */
   int mode=0;
   grib_nearest* nearest=NULL;
   double nearest_lats[4]={0,};
@@ -243,80 +334,92 @@ static int read_grib(FILE* gribfile,char * filename,double *lats,double *lons,in
   int    i = 0;
   int    j = 0;
 
-  /* Open files one per coordinate */
-  for (i=0; i < nbCoords; i++)
+  if ( (h = grib_handle_new_from_file(0,gribfile,&err)) != NULL)
   {
-     sprintf(tempfilename,"/tmp/grib_extract[%g,%g].txt",lats[i],lons[i]);
-     fds[i] = fopen(tempfilename,"w");
-     if(!fds[i])
-     {
-       perror(tempfilename);
-       exit(1);
-     }
-  }
+    /* get date and time to use it in filename */
 
-  while((h = grib_handle_new_from_file(0,gribfile,&err)) != NULL) 
-  {
+    /* get date */
+    GRIB_CHECK(grib_get_string(h,"date",date,&datelen),date);
+  
+    /* get time */
+    GRIB_CHECK(grib_get_string(h,"time",time,&timelen),date);
 
-    grib_count++;
-    if(!h) 
+    /* Open files one per coordinate */
+    for (i=0; i < nbCoords; i++)
     {
-      printf("ERROR: Unable to create grib handle\n");
-      exit(1);
+      sprintf(tempfilename,"%s/gextract_%s%s_[%g,%g].data",directory,date,time,lats[i],lons[i]);
+      fds[i] = fopen(tempfilename,"w");
+      if(!fds[i])
+      {
+        perror(tempfilename);
+        exit(EXIT_FAILURE);
+      }
     }
 
-    /* get grib param or short_name */
-    snlen=MAX_VAL_LEN;
-    GRIB_CHECK(grib_get_string(h,"short_name",short_name,&snlen),short_name);
-    /* printf("short_name = %s\n",short_name); */
-
-    if ( (strncmp(short_name,"U",snlen)==0) || (strncmp(short_name,"V",snlen)==0) || (strncmp(short_name,"T",snlen)==0))
+    do
     {
-       /* get level */
-       GRIB_CHECK(grib_get_string(h,"lev",level,&levlen),level);
-       levlen = MAX_VAL_LEN;
 
-       /* get date */
-       GRIB_CHECK(grib_get_string(h,"date",date,&datelen),date);
-       datelen = MAX_VAL_LEN;
+      grib_count++;
+      if(!h) 
+      {
+        printf("ERROR: Unable to create grib handle\n");
+        exit(1);
+      }
 
-       /* get time */
-       GRIB_CHECK(grib_get_string(h,"time",time,&timelen),date);
-       timelen = MAX_VAL_LEN;
+      /* get grib param or short_name */
+      snlen=MAX_VAL_LEN;
+      GRIB_CHECK(grib_get_string(h,"short_name",short_name,&snlen),short_name);
+      /* printf("short_name = %s\n",short_name); */
 
-       /* For each of the coordinates */
-       for (j=0; j < nbCoords; j++)
-       {
-
-          /* get nearest value from nearest grib point */
-          mode=GRIB_NEAREST_SAME_GRID |  GRIB_NEAREST_SAME_POINT;
-
-          /* create nearest structure */
-          if (!nearest) nearest=grib_nearest_new(h,&err);
-            GRIB_CHECK(err,0);
-
-          GRIB_CHECK(grib_nearest_find(nearest,h,lats[j],lons[j],mode,nearest_lats,nearest_lons,values,distances,indexes,&size),0);
-
-          fprintf(fds[j],"-- GRIB N. %d --: date=%s, time=%s, param=%s, lev=%s, lat=%.2f, lon=%.2f, distance=%g, values=%g - \n",grib_count,date,time,short_name,level ,nearest_lats[0],nearest_lons[0],distances[0],values[0]);
+      if ( (strncmp(short_name,"U",snlen)==0) || (strncmp(short_name,"V",snlen)==0) || (strncmp(short_name,"T",snlen)==0))
+      {
+         /* get level */
+         GRIB_CHECK(grib_get_string(h,"lev",level,&levlen),level);
+         levlen = MAX_VAL_LEN;
+  
+         /* get date */
+         GRIB_CHECK(grib_get_string(h,"date",date,&datelen),date);
+         datelen = MAX_VAL_LEN;
+  
+         /* get time */
+         GRIB_CHECK(grib_get_string(h,"time",time,&timelen),date);
+         timelen = MAX_VAL_LEN;
+  
+         /* For each of the coordinates */
+         for (j=0; j < nbCoords; j++)
+         {
+  
+            /* get nearest value from nearest grib point */
+            mode=GRIB_NEAREST_SAME_GRID |  GRIB_NEAREST_SAME_POINT;
+  
+            /* create nearest structure */
+            if (!nearest) nearest=grib_nearest_new(h,&err);
+              GRIB_CHECK(err,0);
+  
+            GRIB_CHECK(grib_nearest_find(nearest,h,lats[j],lons[j],mode,nearest_lats,nearest_lons,values,distances,indexes,&size),0);
+  
+            fprintf(fds[j],"-- GRIB N. %d --: date=%s, time=%s, param=%s, lev=%s, lat=%.2f, lon=%.2f, distance=%g, values=%g - \n",grib_count,date,time,short_name,level ,nearest_lats[0],nearest_lons[0],distances[0],values[0]);
           
-          if (nearest)
-          { 
-             grib_nearest_delete(nearest);       
-             nearest = NULL;
-          }
-          /*for (i=0;i<4;i++) 
-          {        
-            printf("%d %.2f %.2f %g %g - ", (int)indexes[i],lats[i],lons[i],distances[i],values[i]);
-            printf("\n");
-          }
-          */
-       }
+            if (nearest)
+            { 
+               grib_nearest_delete(nearest);       
+               nearest = NULL;
+            }
+            /*for (i=0;i<4;i++) 
+            {        
+              printf("%d %.2f %.2f %g %g - ", (int)indexes[i],lats[i],lons[i],distances[i],values[i]);
+              printf("\n");
+            }
+            */
+         }
+      }
+
+
+      /* reinit snlen as it is a inout param */
+      snlen = MAX_VAL_LEN;
+
     }
-
-
-    /* reinit snlen as it is a inout param */
-    snlen = MAX_VAL_LEN;
-
+    while( (h = grib_handle_new_from_file(0,gribfile,&err)) != NULL);
   }
 
   if (h) grib_handle_delete(h);
